d3b37636ed2738bb67280e50cac3068d32e02d2a
Daniel Segovia <daniel@dabici.mx>
2017-03-22 21:47:07 -0600

    twitter login working..

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,7 +1,36 @@
 module.exports = function(app, passport){
 
-	app.get('/', function(req, res){
-		res.end('hello world!')
+	function isLoggedIn(req, res, next){
+		if(req.isAuthenticated()){
+			return next()
+		}else{
+			res.redirect('/auth/twitter')
+		}
+	}
+
+	app.use(function(req, res, next){
+		res.locals.user = req.user
+		next()
 	})
+
+
+	app.get('/', function(req, res){
+		res.render('index')
+	})
+
+	app.get('/auth/twitter', passport.authenticate('twitter'))
+	app.get('/auth/twitter/callback', 
+		passport.authenticate('twitter', {
+			successRedirect: '/',
+			failureRedirect: '/login'
+		})
+	)
+
+	app.get('/logout', function(req, res){
+		req.logout()
+		res.redirect('/')
+	})
+
+
 
 }
